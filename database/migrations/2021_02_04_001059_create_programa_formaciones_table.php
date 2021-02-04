390cb179e07513b3dd45519f75b7e94cc6e3de64
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-02-03 21:24:49 -0500

    2nd commit: 2 migratios created

--- database/migrations/2021_02_04_001059_create_programa_formaciones_table.php
+++ database/migrations/2021_02_04_001059_create_programa_formaciones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProgramaFormacionesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('programa_formaciones', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('nombre');
+            $table->string('sigla',10);
+            $table->string('codigo');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('programa_formaciones');
     }
 }
