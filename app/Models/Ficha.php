d669310d185414b15926c7a7886534880b90bf66
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-02-08 16:38:27 -0500

    dsgdsgsdfg

--- app/Models/Ficha.php
+++ app/Models/Ficha.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Ficha extends Model
 {
     use HasFactory;
+    protected $table="fichas";
+    protected $primaryKey="id";
+    protected $fillable=[
+        'num_caracterizacion','estado','jornada_id','programa_formacion_id'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JornadaController;
 use App\Http\Controllers\ProgramaFormacionController;
+use App\Http\Controllers\FichaController;
 
 Route::get('jornadas/{jornada}/delete',JornadaController::class)->name('jornadas.delete');
 
 Route::resource('jornadas',JornadaController::class);
 
 Route::resource('programaformaciones',ProgramaFormacionController::class);
+
+Route::resource('fichas',FichaController::class);
